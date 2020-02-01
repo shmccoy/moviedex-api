40dcab1e51bd0b0ea5775add917586c6c3356dbf
Sean M <53903852+shmccoy@users.noreply.github.com>
2020-02-01 02:09:01 -0500

    Prep for Heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const cors = require('cors')
 const helmet = require('helmet')
 const moviedex = require('./movie-data.json')
 
-console.log(process.env.API_TOKEN)
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -50,7 +50,17 @@ function handleGetMovie(req, res) {
 
 app.get('/movie', handleGetMovie)
 
-const PORT = 8000
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
